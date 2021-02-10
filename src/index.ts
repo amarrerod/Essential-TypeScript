f56ed7b1555e259d10e0e54903fcf08eb516215d
Alejandro Marrero <amarrerd@ull.edu.es>
2021-02-10 12:43:56 +0000

    feat: Adds inquirer

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 
 import { TodoItem } from "./todoItem";
 import { TodoCollection } from "./todoCollection";
+import * as inquirer from 'inquirer';
 
 let toDos: TodoItem[] = [
     new TodoItem(1, "Buy Flowers"),
@@ -19,3 +20,30 @@ collectionOfToDos.getTodoItems(false).forEach(item => item.printDetails());
 console.log('Removing completed tasks');
 collectionOfToDos.removeComplete();
 collectionOfToDos.getTodoItems(true).forEach(item => item.printDetails()); */
+
+// Gestion de los comandos de los usuarios
+function displayTodoList(): void {
+    console.log(`${collectionOfToDos.userName}'s TodoList` + ` (${collectionOfToDos.getItemCounts().incomplete} items to do)`);
+    collectionOfToDos.getTodoItems(true).forEach(item => item.printDetails());
+}
+
+enum Commands {
+    Quit = 'Quit'
+}
+
+const promptUser = (): void => {
+    console.clear();
+    displayTodoList();
+    inquirer.prompt({
+        type: 'list',
+        name: 'command',
+        message: 'Choose an option',
+        choices: Object.values(Commands)
+    }).then(answers => {
+        if (answers['command'] != Commands.Quit) {
+            promptUser();
+        }
+    })
+}
+
+promptUser();
